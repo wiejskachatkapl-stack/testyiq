0c114d7179e8705a035e8e480cea33c43203163c
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-09 20:51:15 +0200

    Add files via upload

---
+++ README_GITHUB.txt
@@ -0,0 +1,12 @@
+IQ Challenge PWA v1
+
+Wrzucasz na GitHub Pages całą zawartość katalogu do repozytorium.
+Pliki wymagane do PWA:
+- index.html
+- quiz.html
+- manifest.webmanifest
+- service-worker.js
+- assets/icons/icon-192.png
+- assets/icons/icon-512.png
+
+Po opublikowaniu na GitHub Pages aplikacja powinna mieć opcję „Dodaj do ekranu głównego” / instalacji PWA.
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'iq-challenge-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './quiz.html',
+  './manifest.webmanifest',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
