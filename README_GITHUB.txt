022a9d47f6767ae8799ee97527be858b1a0553fb
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-10 08:15:16 +0200

    Add files via upload

--- README_GITHUB.txt
+++ README_GITHUB.txt
@@ -1,5 +1,4 @@
-IQ Challenge PWA v5
-
-Wgraj wszystkie pliki z katalogu do repozytorium GitHub Pages.
-Po podmianie na telefonie odśwież stronę i w razie potrzeby usuń starą aplikację PWA z ekranu telefonu, a potem dodaj ponownie.
-Ta wersja ma nową nazwę cache: iq-challenge-v5-portrait-start.
+IQ CHALLENGE PWA v6
+1. Wgraj całą zawartość katalogu do repozytorium GitHub.
+2. Włącz GitHub Pages z gałęzi main i katalogu root.
+3. Po aktualizacji usuń starą wersję aplikacji z telefonu lub wyczyść dane witryny, aby odświeżyć cache PWA.
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE='iq-challenge-v5-portrait-start';
-const FILES=['./','./index.html?v=5','./index.html','./quiz.html?v=5','./quiz.html','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+const CACHE='iq-challenge-v6-20260710';const FILES=['./','./index.html','./quiz.html','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
