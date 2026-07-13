ccbe5aa454067bc933e32092719c2d8081c42919
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-13 11:31:51 +0200

    Add files via upload

--- README_GITHUB.txt
+++ README_GITHUB.txt
@@ -1,1 +1,15 @@
-Wgraj wszystkie pliki i katalog assets do głównego katalogu repozytorium GitHub Pages. Wersja v9 ma nową pamięć cache PWA. Po publikacji odśwież stronę lub ponownie uruchom zainstalowaną aplikację.
+IQ CHALLENGE v10 – GENERATOR
+
+1. Rozpakuj ZIP.
+2. Wgraj wszystkie pliki z katalogu głównego do repozytorium GitHub Pages.
+3. Poczekaj 1–3 minuty i odśwież stronę.
+4. Przy zainstalowanej starszej wersji PWA zamknij ją całkowicie i uruchom ponownie.
+
+Nowości:
+- automatyczny generator pytań,
+- 5 typów zadań,
+- losowe warianty przy każdym teście,
+- adaptacyjna trudność 1–10,
+- zawsze 5 odpowiedzi i jedna poprawna,
+- responsywność telefon/komputer,
+- przykładowa reklama przed wynikiem.
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='iq-challenge-v9-showcase';const FILES=['./','./index.html','./quiz.html','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+const CACHE='iq-challenge-v10-generator';
+const ASSETS=['./','./index.html','./quiz.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
